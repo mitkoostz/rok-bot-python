a8ccd3121227875b30f2a2a83a3c5765db7ceb17
mitkoostz <todorinio123@gmail.com>
2025-01-24 18:42:15 +0200

    refactoring and improvements

--- BarbarianBot.py
+++ BarbarianBot.py
@@ -1,9 +1,65 @@
 from Cordinates import Coordinates
+import time
 
 
 class BarbarianBot:
+
     def __init__(self, bot):
         self.bot = bot
+        self.initialBarbarianLevelStart = 11
+
+    def run_bot(self):
+        while True:
+            isMarching = self.AreTroopsMarching()
+            isInCombat = self.AreTroopsInCombat()
+            print("Marching: " + str(isMarching))
+            print("Combat: " + str(isInCombat))
+            time.sleep(1)
+            if not isMarching and not isInCombat:
+                self.AttackBarbarians()
+            while self.AreTroopsMarching():
+                print("Marching to barbarians which will be attacked.")
+                time.sleep(1)
+            print("Marching is done.")
+            time.sleep(2)
+            while self.AreTroopsInCombat():
+                print("In combat.")
+                time.sleep(1)
+
+    def AttackBarbarians(self):
+        self.ClickSearchButton()
+        time.sleep(1)
+        self.SearchForBarbarians()
+        time.sleep(1)
+
+        # Handle barbarians not found on the map
+        isSearchButtonPresent = self.IsSearchButtonPresent()
+        time.sleep(0.5)
+        print("Search button present: " + str(isSearchButtonPresent))
+        if isSearchButtonPresent:
+            if self.initialBarbarianLevelStart < 18:
+                self.IncreaseBarbarianLevel()
+                self.initialBarbarianLevelStart += 1
+                print("Barbarians level increased to: " + str(self.initialBarbarianLevelStart))
+            else:
+                self.DecreaseBarbarianLevel()
+                self.initialBarbarianLevelStart = 10
+                print("Barbarians level reset to: " + str(self.initialBarbarianLevelStart))
+            time.sleep(1)
+            self.SearchForBarbarians()
+
+        if isSearchButtonPresent:
+            time.sleep(2)
+        else:
+            time.sleep(1)
+        self.MarkTheBarbarians()
+        time.sleep(0.5)
+        self.ClickAttackButton()
+        time.sleep(0.5)
+        self.SelectTroops()
+        time.sleep(0.5)
+        self.MarchAndStartAttack()
+        time.sleep(1)
 
     def ClickSearchButton(self):
         self.bot.click_button(*Coordinates.SEARCH_BAR)  # Click on the search bar
--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 from BlueStacksAppChecker import BlueStacksAppChecker
 from GameBot import GameBot
 from BarbarianBot import BarbarianBot
-from Cordinates import Coordinates
-import time
 
 
 def main():
     startBot = True
-    initialBarbarianLevelStart = 11
     if BlueStacksAppChecker.is_bluestacks_running():
         print("BlueStacks is running.")
         bot = GameBot()
@@ -15,56 +12,7 @@ def main():
         if bot.connect_device():
             bot.set_resolution(1280, 720)
             if startBot:
-                while True:
-                    isMarching = barbarian_bot.AreTroopsMarching()
-                    isInCombat = barbarian_bot.AreTroopsInCombat()
-                    print("Marching: " + str(isMarching))
-                    print("Combat: " + str(isInCombat))
-                    time.sleep(1)
-
-                    if isMarching == False and isInCombat == False:
-                        time.sleep(2)
-                        barbarian_bot.ClickSearchButton()
-                        time.sleep(1)
-                        barbarian_bot.SearchForBarbarians()
-                        time.sleep(1)
-
-                        # Handle barbarians not found in the map...
-                        isSearchButtonPresent = barbarian_bot.IsSearchButtonPresent()
-                        time.sleep(0.5)
-                        print("Search button present: " + str(isSearchButtonPresent))
-                        if isSearchButtonPresent:
-                            if initialBarbarianLevelStart < 18:
-                                barbarian_bot.IncreaseBarbarianLevel()
-                                initialBarbarianLevelStart += 1
-                                print("Barbarians level increased to: " + str(initialBarbarianLevelStart))
-                            else:
-                                barbarian_bot.DecreaseBarbarianLevel()
-                                initialBarbarianLevelStart = 10
-                                print("Barbarians level reset to: " + str(initialBarbarianLevelStart))
-                            time.sleep(1)
-                            barbarian_bot.SearchForBarbarians()
-
-                        if isSearchButtonPresent:
-                            time.sleep(2)
-                        else:
-                            time.sleep(1)
-                        barbarian_bot.MarkTheBarbarians()
-                        time.sleep(0.5)
-                        barbarian_bot.ClickAttackButton()
-                        time.sleep(0.5)
-                        barbarian_bot.SelectTroops()
-                        time.sleep(0.5)
-                        barbarian_bot.MarchAndStartAttack()
-                        time.sleep(1)
-                    while barbarian_bot.AreTroopsMarching():
-                        print("Marching to barbarians which will be attacked.")
-                        time.sleep(1)
-                    print("Marching is done.")
-                    time.sleep(2)
-                    while barbarian_bot.AreTroopsInCombat():
-                        print("In combat.")
-                        time.sleep(1)
+                barbarian_bot.run_bot()
             else:
                 print("Bot will not start!!!")
     else:
